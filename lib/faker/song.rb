aa2ddb31286bde10966ed037906afeb24cc103ad
Christopher Fong <christopher.m.fong@gmail.com>
2013-09-27 22:42:30 -0700

    debugged, should work properly now

--- lib/faker/song.rb
+++ lib/faker/song.rb
@@ -3,48 +3,38 @@ module Faker
   class Song < Base
     class << self
 
-      def lyric
-        translate('faker.song.slapchop').sample
+      def lyric(punc = "")
+        translate('faker.song.slapchop').sample + punc
       end
 
-      def lyrics(num = 3)
-        (translate('faker.song.slapchop')[]).shuffle[0, resolve(num)]
+      def sentence
+        lyric(rand_punc)
       end
 
-      def sentence(lyric_count = 4, random_lyrics_to_add = 6)
-        lyrics(lyric_count + rand(random_lyrics_to_add.to_i).to_i).join(' ').capitalize + '.'
+      def paragraph(sentences = 1, random_sentences = 4)
+        para = ""
+        (sentences + rand(random_sentences)).times {para += (sentence + " ")}
+        para.chop
       end
 
-      def sentences(sentence_count = 3)
-        [].tap do |sentences|
-          1.upto(resolve(sentence_count)) do
-            sentences << sentence(3)
-          end
+      def paragraphs(paras = 2, random_paras = 4)
+        text = ""
+        (paras + rand(random_paras)).times {text += (paragraph + "\n")}
+        text.chop
+      end
+
+      private
+
+      #simple version, a more complex text constructor could do more with conditional logic
+      #e.g. making sure sentences never end with a comma unless another sentence follows
+      def rand_punc
+        case rand(4)
+          when 0 then "..."
+          when 1 then "?"
+          when 2 then "!"
+          when 3 then "."
+          else "."
         end
-      end
-
-      def paragraph(sentence_count = 3, random_sentences_to_add = 3)
-        sentences(resolve(sentence_count) + rand(random_sentences_to_add.to_i).to_i).join(' ')
-      end
-
-      def paragraphs(paragraph_count = 3)
-        [].tap do |paragraphs|
-          1.upto(resolve(paragraph_count)) do
-            paragraphs << paragraph(3)
-          end
-        end
-      end
-    end
-
-  private
-
-    # If an array or range is passed, a random value will be selected.
-    # All other values are simply returned.
-    def self.resolve(value)
-      case value
-      when Array then value[rand(value.size)]
-      when Range then rand((value.last+1) - value.first) + value.first
-      else value
       end
     end
   end
